0f49b43acd4337bf8a53391fedf813edb97c57ac
seoji <idw5780@dgsw.hs.kr>
2019-01-24 17:25:17 +0900

    [ADD] eslint setting

---
+++ backend/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "rules": {
+      "linebreak-style": ["error", "windows"],
+      "no-console": "off",
+      "no-param-reassign": "off",
+      "guard-for-in": "off",
+  }
+};

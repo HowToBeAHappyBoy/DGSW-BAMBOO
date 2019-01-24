c099ace0050782e8c3bcae8f8453abf6b42d76af
seoji <idw5780@dgsw.hs.kr>
2019-01-24 18:14:52 +0900

    [ADD] admiin model

---
+++ backend/src/database/models/admin.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const {
+  secret,
+} = require('config.json/serverconfig.json');
+
+const Admin = Schema({
+  id: { type: String, require: true, unique: true },
+  password: { type: String, require: true, unique: false },
+  name: { type: String, require: true, unique: false },
+}, {
+  collection: 'admin',
+});
+Admin.statics.signup = (id, pw, name) => {
+  const cipher = crypto.createCipher('aes-256-cbc', secret);
+  let resultId = cipher.update(id, 'utf8', 'base64');
+  resultId += cipher.final('base64');
+
+
+  const resultPw = crypto.createHmac('sha1', secret)
+    .update(pw)
+    .digest('base64');
+
+
+  return new this({
+    id: resultId,
+    password: resultPw,
+    name,
+  }).save();
+};
+
+Admin.statics.findById = (_id) => {
+  const cipher = crypto.createCipher('aes-256-cbc', secret);
+  let id = cipher.update(_id, 'utf8', 'base64');
+  id += cipher.final('base64');
+  return this.findOne({ id }).exec();
+};
+
+Admin.methods.checkPassword = (pw) => {
+  const resultPw = crypto.createHmac('sha1', secret)
+    .update(pw)
+    .digest('base64');
+  return this.password === resultPw;
+};
+
+// eslint-disable-next-line func-names
+Admin.methods.urgentToken = function () {
+  return new Promise((resolve, reject) => {
+    jwt.sign({
+      // eslint-disable-next-line no-underscore-dangle
+      _id: this._id,
+      admin: this.name,
+    },
+    secret, {
+      issuer: 'dgswbamboo.oa.to',
+      subject: 'usertoken',
+      expiresIn: '3h',
+    }, (err, token) => {
+      if (err) reject(err);
+      resolve(token);
+    });
+  });
+};
+
+module.exports = mongoose.model('admin', Admin);

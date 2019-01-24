54c98088513ecb0aca201277ff0fb01faf4cd439
seoji <idw5780@dgsw.hs.kr>
2019-01-24 18:14:42 +0900

    [ADD] connect database

---
+++ backend/src/database/index.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const {
+  dbUrl,
+} = require('config/serverconfig.json');
+
+module.exports = () => {
+  mongoose.Promise = global.Promise;
+  mongoose.connect(dbUrl);
+  mongoose.connection.on('error', console.error.bind(console, 'mongoose connection error.'));
+  mongoose.connection.on('open', () => {
+    console.log('connected db');
+  });
+  mongoose.connection.on('disconnected', () => {
+    console.log('disconnected db');
+  });
+};

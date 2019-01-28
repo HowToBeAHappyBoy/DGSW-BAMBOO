eb71e7742ddb8faac0fabffac50fef49b822f124
seoji <idw5780@dgsw.hs.kr>
2019-01-28 13:38:23 +0900

    [ADD] POST admin/reject
    거절 api 추가

--- backend/src/api/admin/admin.ctrl.js
+++ backend/src/api/admin/admin.ctrl.js
@@ -42,3 +42,58 @@ exports.count = async (req, res) => {
     res.status(200).json(result);
   }
 };
+exports.reject = async (req, res) => {
+  const {
+    idx,
+    reason,
+  } = req.body;
+  const {
+    admin,
+  } = req.decoded;
+  try {
+    const post = await waitPost.findOne({ idx });
+    if (!post || post.isChange === true) {
+      const result = {
+        status: 404,
+        desc: '해당 idx의 대기 글이 없어요',
+      };
+      res.status(200).json(result);
+      return;
+    }
+    const {
+      content,
+      writeDate,
+      type,
+      writerName,
+      writerPicture,
+      writerUrl,
+      imgs,
+    } = post;
+
+    await rejectPost.create({
+      idx,
+      content,
+      writeDate,
+      writerName,
+      writerPicture,
+      writerUrl,
+      type,
+      reason,
+      admin,
+      imgs,
+    });
+    await waitPost.updateOne({ idx }, { $set: { isChange: true } });
+    const result = {
+      status: 200,
+      desc: 'successful request',
+    };
+    res.status(200).json(result);
+  } catch (error) {
+    const result = {
+      status: 500,
+      error: error.message,
+    };
+    console.log(error.message);
+    res.status(200).json(result);
+  }
+};
--- backend/src/api/admin/index.js
+++ backend/src/api/admin/index.js
@@ -5,6 +5,7 @@ const adminCtrl = require('./admin.ctrl');
 const cert = require('./cert');
 
 router.route('/count/:type').get(certFunc, adminCtrl.count);
+router.route('/reject').post(certFunc, adminCtrl.reject);
 router.use('/cert', cert);
 
 module.exports = router;

9777cf3a6b6cb6666f286b84c73070dde764bb94
seoji <idw5780@dgsw.hs.kr>
2019-01-30 11:14:48 +0900

    [FIX] database conection
    경고 메시지가 뜨길래 고쳤어요

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -5,7 +5,10 @@ const {
 
 module.exports = () => {
   mongoose.Promise = global.Promise;
-  mongoose.connect(dbUrl);
+  mongoose.connect(dbUrl, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+  });
   mongoose.connection.on('error', console.error.bind(console, 'mongoose connection error.'));
   mongoose.connection.on('open', () => {
     console.log('connected db');

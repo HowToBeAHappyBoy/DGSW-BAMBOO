e7a960eb552e0af55225499501bbebee22eee681
seoji <idw5780@dgsw.hs.kr>
2019-03-08 21:38:44 +0900

    [ADD] App.js Root.js
    기본 설정

---
+++ frontend/src/Root.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import App from 'components/App';
+
+const Root = () => {
+    return (
+        <div>
+            <App/>
+        </div>
+    );
+}
+
+export default Root;

b0c0c908973299f205175e0f1012d4a9b66f63ec
seoji <idw5780@dgsw.hs.kr>
2019-03-12 19:40:10 +0900

    [ADD] Story Component
    Story를 최종적으로 렌더링 하는 컴포넌트

---
+++ frontend/src/components/Story/Story.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from './Story.scss';
+import classNames from 'classnames/bind';
+import {
+    PANDA
+} from 'config/config.json'
+
+const cx = classNames.bind(styles);
+
+const Story = ({ story }) => {
+    const writeDate = new Date(story.writeDate);
+    return (
+        <div className={cx('story')}>
+           <div className={cx('story-header')}>
+                <p className={cx('header-title')}>{story.idx}번째 이야기</p>
+                <p className={cx('header-date')}>{writeDate.toLocaleString()} 제보</p>
+           </div>
+           <div className={cx('story-body')}>
+                <p className={cx('body-content')}>{story.content}</p>
+           </div>
+           <div className={cx('story-footer')}>
+                <div className={cx('footer-profile')}>
+                    <img src={story.type ? story.writerPicture : PANDA} alt="제보자 프로필 사진"/>
+                    <a href={story.writerUrl} target="_blank" rel="noopener noreferrer">
+                        <p className={cx('profile-name')}>{story.type ? `${story.writerName} 님` : "익명의 판다" } 제보</p>
+                    </a>
+                </div>
+           </div>
+        </div>
+    );
+}
+
+export default Story;

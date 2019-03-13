820677319e1c27d9ecae995f4f07083a5747662e
seoji <idw5780@dgsw.hs.kr>
2019-03-13 16:25:49 +0900

    [FIX] ImageViewer Component
    useState로 Modal 상태를 관리하는 코드들 추가

--- frontend/src/components/ImageViewer/ImageViewer.js
+++ frontend/src/components/ImageViewer/ImageViewer.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './ImageViewer.scss';
 import classNames from 'classnames/bind';
 import { FaImage, FaImages } from "react-icons/fa";
+import ImageModal from 'components/ImageModal';
 
 const cx = classNames.bind(styles);
 
 const ImageViewer = ({ images }) => {
+    const [show, setShow] = useState(false);
     return (
-        <div className={cx('image-viewer')}>
+        <>
+        <div className={cx('image-viewer')} onClick={()=>{setShow(true)}}>
             {images.length === 1 ? <FaImage size={40}/> : <FaImages size={40} />}
             <p className={cx('image-title')}>
                 {images.length} 장의 사진 포함
             </p>
         </div>
+        {show && <ImageModal images={images} change={setShow} show={show}/>}
+        </>
     );
 };
 

7ca9e8bdbb18c8f19a068d447c9d2f30fdcc8dbd
seoji <idw5780@dgsw.hs.kr>
2019-03-15 10:09:39 +0900

    [FIX] StoryStore
    getCount 총 글이 몇개인지 체크
    getMoreStory 스크롤하면 실행됨, 5개의 글을 더 불러옴

--- frontend/src/stores/Story/StoryStore.js
+++ frontend/src/stores/Story/StoryStore.js
@@ -7,19 +7,60 @@ import { autobind } from 'core-decorators';
 class StoryStore {
     @observable storyList = [];
     @observable stat = 'pending';
+    @observable moreStat;
+    @observable count = 0;
+    @observable maxCount;
 
     @asyncAction
-    async *getStory (page) {
+    async *getCount () {
         try{
-            const { data, status } = yield StoryRepository.getStory(page);
+            const { data, status } = yield StoryRepository.getCount();
             if (status === 200) {
-                this.stat = 'success';
-                this.storyList = data.post.map(story => story);
+                this.maxCount = data.count;
             } else {
                 this.stat = 'error';
             }
-        } catch (error) {
+        } catch (error) {   
             this.stat = 'error';
+        }
+    }
+    @asyncAction
+    async *getStory () {
+        try{
+            const { data, status } = yield StoryRepository.getStory(this.count);
+            if (status === 200) {
+                setTimeout(()=>{
+                    this.stat = 'success';
+                    this.storyList = data.post.map(story => story);
+                    this.count += 5;
+                }, 1000);
+            } else {
+                this.stat = 'error';
+            }
+        } catch (error) {   
+            this.stat = 'error';
+        }
+    }
+    @asyncAction
+    async *getMoreStory () {
+        if(this.count > this.maxCount || this.moreStat === 'pending'){
+            return;
+        }
+        this.moreStat = 'pending';
+        try{
+            const { data, status } = yield StoryRepository.getStory(this.count);
+            if (status === 200) {
+                setTimeout(()=>{
+                    this.moreStat = 'success';
+                    const newList = this.storyList.concat(data.post);
+                    this.storyList = newList;
+                    this.count += 5;
+                })
+            } else {
+                this.moreStat = 'error';
+            }
+        } catch (error) {   
+            this.moreStat = 'error';
         }
     }
 }

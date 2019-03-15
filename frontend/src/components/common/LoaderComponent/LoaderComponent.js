aabf1921c55f998b20d2f32b15501fb26dd9579c
seoji <idw5780@dgsw.hs.kr>
2019-03-15 10:04:40 +0900

    [FIX] LoaderComponent
    재사용성을 위해 height를 props로 받아서 적용

--- frontend/src/components/common/LoaderComponent/LoaderComponent.js
+++ frontend/src/components/common/LoaderComponent/LoaderComponent.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './LoaderComponent.scss';
+import styled from 'styled-components'
 
 const cx = classNames.bind(styles);
 
-const LoaderComponent = () => {
+const LoaderComponent = ({ height }) => {
     return (
-        <div className={cx('container')}>
+        <Container height={height}>
             <img src="https://loading.io/spinners/rolling/lg.curve-bars-loading-indicator.gif" alt="spiner" className={cx('spiner')}/>
-        </div>
+        </Container>
     );
 };
+
+const Container = styled.div `
+    display: flex;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    background-color: transparent;
+    height : ${(props)=>props.height}
+`;
 
 export default LoaderComponent;

7439e9bbce358e9a2418d196bf8d3f4aba5a3b9f
seoji <idw5780@dgsw.hs.kr>
2019-03-31 18:50:12 +0900

    [FiX] Story

--- frontend/src/components/Story/Story.js
+++ frontend/src/components/Story/Story.js
@@ -1,37 +1,47 @@
 import React from 'react';
 import styles from './Story.scss';
 import classNames from 'classnames/bind';
-import {
-    PANDA
-} from 'config/config.json'
-import ImageViewer from 'components/ImageViewer';
+import { PANDA } from 'config/config.json';
+import ImageViewer from 'components/image/ImageViewer';
 
 const cx = classNames.bind(styles);
 
-const Story = ({ story }) => {
-    const writeDate = new Date(story.writeDate);
-    return (
-        <div className={cx('story')}>
-           <div className={cx('story-header')}>
-                <h1 className={cx('header-title')}>{story.idx}번째 이야기</h1>
-                <p className={cx('header-date')}>{writeDate.toLocaleString()} 제보</p>
-           </div>
-           <div className={cx('story-body')}>
-                <p className={cx('body-content')}>{story.content}</p>
-                { story.imgs.length !== 0 && <ImageViewer images={story.imgs}/> }
-           </div>
-           <div className={cx('story-footer')}>
-                <div className={cx('footer-profile')}>
-                    <img src={story.type ? story.writerPicture : PANDA} alt="제보자 프로필 사진"/>
-                    <a href={story.writerUrl} target="_blank" rel="noopener noreferrer">
-                        <p className={cx('profile-name', { 'profile-name-underline': story.type })}>
-                            {story.type ? `${story.writerName} 님` : "익명의 판다" } 제보
-                        </p>
-                    </a>
-                </div>
-           </div>
+const Story = ({ story, width }) => {
+  const writeDate = new Date(story.writeDate);
+  const allowDate = new Date(story.allowDate);
+  const content = story.content.replace('\r\n', '<br/>');
+  return (
+    <div className={cx('story')} style={{ maxWidth: width }}>
+      <div className={cx('story-header')}>
+        <h1 className={cx('header-title')}>{story.idx}번째 이야기</h1>
+      </div>
+      <div className={cx('story-body')}>
+        <pre className={cx('body-content')}>{content}</pre>
+        {story.imgs.length !== 0 && <ImageViewer images={story.imgs} />}
+      </div>
+      <div className={cx('story-footer')}>
+        <div className={cx('footer-profile')}>
+          <img
+            src={story.type ? story.writerPicture : PANDA}
+            alt="제보자 프로필 사진"
+          />
+          {story.type ? (
+            <a href={story.writerUrl} target="_blank" rel="noopener noreferrer">
+              <p className={cx('profile-name', 'profile-name-underline')}>
+                {story.writerName}님 제보
+              </p>
+            </a>
+          ) : (
+            <p className={cx('profile-name')}>익명의 판다 제보</p>
+          )}
         </div>
-    );
-}
+        <div className={cx('footer-date')}>
+          <p className={cx('write-date')}>{writeDate.toLocaleString()} 제보</p>
+          <p className={cx('allow-date')}>{allowDate.toLocaleString()} 승인</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Story;

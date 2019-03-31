560567cb58cf81689360ff102f52d4e4290be214
seoji <idw5780@dgsw.hs.kr>
2019-03-31 19:38:54 +0900

    [FIX] LoaderComponents

--- frontend/src/components/common/LoaderComponent/LoaderComponent.js
+++ frontend/src/components/common/LoaderComponent/LoaderComponent.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './LoaderComponent.scss';
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const cx = classNames.bind(styles);
 
 const LoaderComponent = ({ height }) => {
-    return (
-        <Container height={height}>
-            <img src="https://loading.io/spinners/rolling/lg.curve-bars-loading-indicator.gif" alt="spiner" className={cx('spiner')}/>
-        </Container>
-    );
+  return (
+    <Container height={height}>
+      <img
+        src="https://loading.io/spinners/rolling/lg.curve-bars-loading-indicator.gif"
+        alt="spiner"
+        className={cx('spiner')}
+      />
+    </Container>
+  );
 };
 
-const Container = styled.div `
-    display: flex;
-    flex: 1;
-    justify-content: center;
-    align-items: center;
-    background-color: transparent;
-    height : ${(props)=>props.height}
+const Container = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: transparent;
+  height: ${props => props.height};
 `;
 
 export default LoaderComponent;

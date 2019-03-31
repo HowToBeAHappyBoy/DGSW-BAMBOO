557924879d56a58fdf06adda6c03ce5b11865c07
seoji <idw5780@dgsw.hs.kr>
2019-03-31 14:19:00 +0900

    [DELETE] Report Page
    Edit로 대체됨

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { HomePage, ReportPage, EditorPage } from 'pages';
+import { HomePage, EditorPage } from 'pages';
 
 const App = () => {
   return (
     <Switch>
       <Route exact path="/" component={HomePage} />
-      <Route path="/report" component={ReportPage} />
       <Route path="/edit" component={EditorPage} />
     </Switch>
   );

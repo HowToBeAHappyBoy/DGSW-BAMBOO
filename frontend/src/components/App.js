9b0cbf53be859250ec9c420435391a07bcaadae1
seoji <idw5780@dgsw.hs.kr>
2019-04-04 09:17:29 +0900

    [FIX] Route

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { HomePage, EditorPage } from 'pages';
+import { HomePage, EditorPage, AdminPage, NotFoundPage } from 'pages';
 
 const App = () => {
   return (
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route path="/edit" component={EditorPage} />
+      <Route path="/admin" component={AdminPage} />
+      <Route component={NotFoundPage} />
     </Switch>
   );
 };
